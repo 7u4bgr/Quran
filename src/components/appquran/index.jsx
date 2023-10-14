dcab373f391f4a08cb49ce36d1ed000e2cc8c993
banch0u <fikret.okis@gmail.com>
2023-10-14 17:09:56 +0300

    backgrund image fix

--- src/components/appquran/index.jsx
+++ src/components/appquran/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import styles from "./index.module.css";
-import Vector1 from "../../assets/images/vector1.svg";
-import Wrapper from "../UI/wrapper";
+import React from 'react';
+import styles from './index.module.css';
+import Vector1 from '../../assets/images/vector1.svg';
+import Wrapper from '../UI/wrapper';
 const AppQuran = () => {
   return (
     <div className={styles.background}>
@@ -12,10 +12,10 @@ const AppQuran = () => {
           </div>
         </div>
       </Wrapper>
-        <div className={styles.vectors}>
-          <img className={styles.img1} src={Vector1} alt="" />
-          <img className={styles.img2} src={Vector1} alt="" />
-        </div>
+      {/* <div className={styles.vectors}>
+        <img className={styles.img1} src={Vector1} alt='' />
+        <img className={styles.img2} src={Vector1} alt='' />
+      </div> */}
     </div>
   );
 };
